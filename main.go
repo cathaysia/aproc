9e58c1beaba2206b2d597cd070a3f1112503c515
cathaysia <DragonBillow@outlook.com>
2022-03-17 00:11:04 +0800

    refactor(main.go): 优化了程序结构

--- main.go
+++ main.go
@@ -48,8 +48,7 @@ func main() {
 		logrus.Fatalln("请使用超级用户运行此程序")
 	}
 
-	settings, err = lib.GetSettings()
-	if err != nil {
+	if settings, err = lib.GetSettings(); err != nil {
 		logrus.Fatalln(err)
 	}
 
@@ -57,44 +56,32 @@ func main() {
 	signal.Notify(sigInt, syscall.SIGINT)
 	// 监控 /proc 目录的变动
 	watcher := lib.NewProgressWatcher(2000)
-	watchExit := make(chan bool)
-	waitExit := make(chan bool)
+	watcher.Watch()
 
-	go func() {
-		for {
-			select {
-			case event := <-watcher.Event:
-				if event.IsCreate() {
-					logrus.Debugf("%v is Created\n", event.PID)
+	for {
+		select {
+		case event := <-watcher.Event:
+			if event.IsCreate() {
+				logrus.Debugf("%v is Created\n", event.PID)
 
-					if err := createCGroupForPID(event.PID); err != nil {
-						watcher.Error <- err
-					}
-				} else if event.IsDelete() {
-					logrus.Debugf("%v is Deleted\n", event.PID)
-
-					lib.CleanManager()
+				if err := createCGroupForPID(event.PID); err != nil {
+					watcher.Error <- err
 				}
-			case err := <-watcher.Error:
-				logrus.Errorln(err)
-				signal.Notify(sigInt, syscall.SIGINT) // 请求退出进程
-			case <-watchExit: // 等待退出协程
-				waitExit <- true
+			} else if event.IsDelete() {
+				logrus.Debugf("%v is Deleted\n", event.PID)
+
+				lib.CleanManager()
+			}
+		case err := <-watcher.Error:
+			logrus.Errorln(err)
+			signal.Notify(sigInt, syscall.SIGINT) // 请求退出进程
+		case res := <-sigInt:
+			if res == syscall.SIGINT {
+				watcher.Exit()
+				<-watcher.WaitForExit
 
 				return
 			}
 		}
-	}()
-
-	watcher.Watch()
-
-	if <-sigInt == syscall.SIGINT {
-		watcher.Exit()    // 通知 watcher 退出
-		watchExit <- true // 通知轮询协程退出
-
-		<-watcher.WaitForExit // 等待 watcher 退出
-		<-waitExit            // 等待轮询协程退出
-
-		return
 	}
 }
